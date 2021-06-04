2a6748d007e7156008db585d32c8b847568f7aa3
shushikata <shu.42111227@gmail.com>
2021-06-04 17:14:55 +0900

    4-1 CardAddコンポーネントを作成

--- src/components/CardAdd.vue
+++ src/components/CardAdd.vue
@@ -1,12 +1,17 @@
 <template>
-  <form class="addcard" @submit.prevent="addCardToList">
+  <form :class="classList" @submit.prevent="addCardToList">
     <input v-model="body"
         type="text"
         class="text-input"
         placeholder="Add new card"
+        @focusin="startEditing"
+        @focusout="finishEditing"
     >
 
-    <button type="text" class="add-button">
+    <button type="text"
+        class="add-button"
+        v-if="isEditing || bodyExists"
+    >
       Add
     </button>
   </form>
@@ -25,14 +30,43 @@ export default {
   data: function() {
     return {
       body: "",
+      isEditing: false,
+    }
+  },
+
+  computed: {
+    classList() {
+      const classList = ["addcard"]
+      if(this.isEditing) {
+        classList.push("active")
+      }
+
+      if(this.bodyExists) {
+        classList.push("addable")
+      }
+
+      return classList
+    },
+
+    bodyExists() {
+      return this.body.length > 0
     }
   },
 
   methods: {
+
+    startEditing: function() {
+      this.isEditing = true
+    },
+
+    finishEditing: function() {
+      this.isEditing = false
+    },
+
     addCardToList: function() {
       this.$store.dispatch("addCardToList", { body: this.body, listIndex: this.listIndex})
       this.body = ""
-    }
+    },
   }
 }
 </script>
