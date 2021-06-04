7724d9ba7dbc2a9e724064f1f84bb04214a8b909
shushikata <shu.42111227@gmail.com>
2021-06-04 18:05:05 +0900

    4-2 Cardコンポーネントを作成

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -10,6 +10,7 @@
         <list v-for="(item, index) in lists"
             :key="item.id"
             :title="item.title"
+            :cards="item.cards"
             :listIndex="index"
         />
         <list-add/>
--- src/store/index.js
+++ src/store/index.js
@@ -40,6 +40,10 @@ const store = new Vuex.Store({
     addCardToList(state, payload) {
       state.lists[payload.listIndex].cards.push({ body: payload.body })
     },
+
+    removeCardFromList(state, payload) {
+      state.lists[payload.listIndex].cards.splice(payload.cardIndex, 1)
+    },
   },
 
   actions: {
@@ -53,6 +57,10 @@ const store = new Vuex.Store({
 
     addCardToList(context, payload) {
       context.commit("addCardToList", payload)
+    },
+
+    removeCardFromList(context, payload) {
+      context.commit("removeCardFromList", payload)
     },
   },
 
